bba2d5007610de421ec34d2fb11e55c7799c5b5d
Shannon Greenwood <shannon.greenwood@gmail.com>
2021-09-12 13:57:44 -0700

    Gameobject rigid body test

---
+++ Assets/Scripts/TowerCreator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerCreator : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public int cubeMax = 100;
+    public int width = 10;
+    public int length = 10;
+
+    private void Start()
+    {
+        int height = cubeMax;
+        int cubeCount = 0;
+        for (int h = 0; h < height; h++)
+        {
+            for (int w = 0; w < width; w++)
+            {
+                for (int l = 0; l < length; l++)
+                {
+                    if (l != 0 && w != 0 && l != length - 1 && w != width - 1)
+                    {
+                        //do not instantiate a cube, skip to the next values
+                        continue;
+                    }
+
+                    Instantiate(cubePrefab, new Vector3(l, h + 0.5f, w), Quaternion.identity);
+                    cubeCount++;
+                    if (cubeCount >= cubeMax)
+                    {
+                        break;
+                    }
+                }
+                if (cubeCount >= cubeMax)
+                {
+                    break;
+                }
+            }
+            if (cubeCount >= cubeMax)
+            {
+                break;
+            }
+        }
+
+    }
+}
